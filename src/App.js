fbed4f54f73e3162f2a343bf43f5b0a657ade1bd
Hadi <hadibakhshi277@gmail.com>
2021-12-23 13:55:59 +0330

    Two Components modified

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import "./App.css";
+import ExpenseApp from "./Components/ExpenseApp/ExpenseApp";
 
 function App() {
   return (
-    <div className="App h-screen w-screen flex justify-center items-center">
-      <h2 className="bg-slate-200 text-indigo-600 rounded-lg font-bold p-2">Hello world</h2>
+    <div className="App h-screen bg-slate-200 text-zinc-700 flex flex-col items-center w-screen pt-8 px-2">
+      <header>
+        <h2>Expense Tracker</h2>
+      </header>
+      <ExpenseApp/>
     </div>
   );
 }
---
+++ src/Components/TransAction/TransAction.js
@@ -0,0 +1,9 @@
+const TransAction = ({transactions}) => {
+    return (
+       <section className="flex items-center justify-around py-2">
+           TransAction
+       </section>
+    )
+}
+
+export default TransAction
