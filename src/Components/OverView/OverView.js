aea0b50153dcf5a9b0b3d3b757b6e61bebd10a57
Hadi <hadibakhshi277@gmail.com>
2021-12-27 00:36:23 +0330

    Styles updated

--- src/Components/OverView/OverView.js
+++ src/Components/OverView/OverView.js
@@ -6,18 +6,30 @@ const OverView = ({ income, expense, addTransaction }) => {
   return (
     <div>
       <div className="flex justify-around items-center py-2 ">
-        <p>Balance : {income - expense}</p>
+        <p>Balance : $ {income - expense}</p>
         <button
           onClick={() => setIsShow((prevState) => !prevState)}
-          className="bg-zinc-600 px-2 rounded-md text-white"
+          className={`${
+            !isShow
+              ? "bg-violet-500 font-semibold px-2 py-1 rounded-md text-violet-100 hover:bg-violet-700 hover:ring-2 ring-blue-300"
+              : ""
+          } ${
+            isShow
+              ? " bg-red-50 px-2 py-1 rounded-md border border-red-600 text-red-600 hover:bg-red-600 hover:text-red-50"
+              : ""
+          }`}
         >
           {isShow ? "Cancel" : "Add"}
         </button>
       </div>
-      {isShow && <TransactionForm addTransaction={addTransaction}/>}
+      {isShow && <TransactionForm addTransaction={addTransaction} />}
       <div className="flex justify-around items-center py-2">
-        <div>Expense {expense}</div>
-        <div>Income {income}</div>
+        <div className="flex flex-col items-center w-full mx-2 border-2 border-solid border-gray-300">
+          Expense <span className="font-bold text-red-600">$ {expense}</span>
+        </div>
+        <div className="flex flex-col  items-center w-full mx-2 border-2 border-solid border-gray-300">
+          Income <span className="font-bold text-green-600">$ {income}</span>
+        </div>
       </div>
     </div>
   );
