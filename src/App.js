51aba4f19503ec919c3c4c0e38cb7fd3408c21a5
Hadi <hadibakhshi277@gmail.com>
2021-12-28 14:23:33 +0330

    Navbar component added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import "./App.css";
 import ExpenseApp from "./Components/ExpenseApp/ExpenseApp";
+import NavBar from "./Components/NavBar/NavBar";
 
 function App() {
   return (
-    <div className="App h-screen bg-slate-200 text-zinc-700 flex flex-col items-center w-screen pt-8 px-2">
-      <header>
-        <h2>Expense Tracker</h2>
-      </header>
-      <ExpenseApp/>
+    <div>
+      <NavBar />
+      <ExpenseApp />
     </div>
   );
 }
